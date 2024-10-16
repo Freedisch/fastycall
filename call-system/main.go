836779e299871c8660d9ab20278ccd575eb2670a
freedisch <freeproduc@gmail.com>
2024-10-16 17:47:20 +0200

    feat: implement websocket connection for retell
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- call-system/main.go
+++ call-system/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"log"
 
+	"fastycall.com/call/services"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
-	
+
 	if err != nil {
 		log.Fatal("Cannot retrieve call \n")
 	}
+	app := gin.Default()
+	app.POST("/twilio-webhook/:agent_id", services.Twiliowebhookhandler)
+	app.Run("localhost:8081")
 }
---
+++ call-system/services/retell_handler.go
@@ -0,0 +1,67 @@
+package services
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+type Transcripts struct {
+	Role string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Request struct {
+	ResponseID int `json:"response_id"`
+	Transcript []Transcripts `json:"transcript"`
+	InteractionType string `json:"interaction_type"`
+}
+
+type Response struct {
+	ResponseID int `json:"response_id"`
+	Content string `json:"content"`
+	ContentComplete bool `json:"content_complete"`
+	EndCall bool `json:"end_call"`
+}
+
+func Retellwhandler(c *gin.Context){
+	upgrader := websocket.Upgrader{}
+
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response := Response{
+		ResponseID: 0,
+		Content: "fastyCall, what's your emergency?",
+		ContentComplete: true,
+		EndCall: false,
+	}
+
+	err = conn.WriteJSON(response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		messageType, ms, err := conn.ReadMessage()
+		if err != nil {
+			conn.Close()
+			break
+		}
+
+		if messageType == websocket.TextMessage {
+			var msg Request
+			json.Unmarshal(ms, &msg)
+		log.Println(msg)
+		}
+	}
+}
