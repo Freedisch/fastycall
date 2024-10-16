89ebde9bac3fafe43a5e78cddf5df3bc6d373bee
freedisch <freeproduc@gmail.com>
2024-10-16 19:25:24 +0200

    feat: integration between websocket handler and openai
    Signed-off-by: freedisch <freeproduc@gmail.com>

---
+++ call-system/services/server.go
@@ -0,0 +1,110 @@
+package services
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+
+	"fastycall.com/call/utils"
+	"github.com/gorilla/websocket"
+	"github.com/sashabaranov/go-openai"
+)
+
+func HandleWebsocketMessages(msg Request, conn *websocket.Conn) {
+	client := openai.NewClient(utils.GetOpenAISecretKey())
+
+	if msg.InteractionType == "update_only" {
+		log.Println("update interaction, do nothing ")
+		return
+	}
+
+	prompt := GenerateAIRequest(msg)
+	req := openai.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo,
+		Messages: prompt,
+		Stream: true,
+		MaxTokens: 200,
+		Temperature: 1.0,
+	}
+	stream, err := client.CreateChatCompletionStream(context.Background(), req)
+	if err != nil {
+		log.Println(err)
+		conn.Close()
+	}
+	defer stream.Close()
+	var i int
+	for {
+		response, err := stream.Recv()
+		if err != nil {
+			var s string
+			if (errors.Is(err, io.EOF) && i == 0) || (!errors.Is(err, io.EOF)){
+				s = "[ERROR] NO RESPONSE, PLEASE RETRY"
+			}
+
+			if errors.Is(err, io.EOF) && i != 0{
+				s = "\n\n###### [END] ######"
+			}
+			airesponse := Response{
+				ResponseID: msg.ResponseID,
+				Content: s,
+				ContentComplete: false,
+				EndCall: false,
+			}
+
+			out, err := json.Marshal(airesponse)
+			if err != nil {
+				log.Println(err)
+				conn.Close()
+			}
+
+			err = conn.WriteMessage(websocket.TextMessage, out)
+			if err != nil {
+				log.Println(err)
+				conn.Close()
+			}
+			break
+		}
+		if len(response.Choices) > 0 {
+			s := response.Choices[0].Delta.Content
+			airesponse := Response{
+				ResponseID: msg.ResponseID,
+				Content: s,
+				ContentComplete: false,
+				EndCall: false,
+			}
+			log.Println(airesponse)
+
+			out, _ := json.Marshal(airesponse)
+			err = conn.WriteMessage(websocket.TextMessage, out)
+			if err != nil {
+				log.Println(err)
+				conn.Close()
+			}
+		}
+		i = i + 1
+	}
+}
+
+func GenerateAIRequest(msg Request) []openai.ChatCompletionMessage {
+	var airequest  []openai.ChatCompletionMessage
+
+	systemprompt := openai.ChatCompletionMessage{
+		Role: "system",
+		Content: "",
+	}
+
+	airequest = append(airequest, systemprompt)
+	for _, response := range msg.Transcript {
+		var p_response openai.ChatCompletionMessage
+		if response.Role == "agent"{
+			p_response.Role = "assistant"
+		} else {
+			p_response.Role = "user"
+		}
+		p_response.Content = response.Content
+		airequest = append(airequest, p_response)
+	}
+	return airequest
+}
