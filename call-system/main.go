b8f56cd94dbdd776237631ca7ac16331ffe9c4eb
freedisch <freeproduc@gmail.com>
2024-11-25 10:30:57 +0200

    add digital ocean health check
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- call-system/main.go
+++ call-system/main.go
@@ -16,8 +16,10 @@ func main() {
 	// Initialize Firebase
 	utils.InitializeFirebase()
 	defer utils.CloseFirestoreConnection()
+	
 
 	app := gin.Default()
+	app.GET("/", services.HealthCheck)
 	app.Any("/llm-websocket/:call_id", services.Retellwhandler)
 	app.POST("/twilio-webhook/:agent_id", services.Twiliowebhookhandler)
 	app.Run("localhost:8081")
